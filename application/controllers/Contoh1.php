1bd68fa46cf0bb9dd3f1e44a0465001635333210
ReynaldySalam <reynaldysalam54@gmail.com>
2022-03-28 13:31:03 +0700

    LatihanCI

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+  public function index()
+  {
+    echo "<h1>Perkenalkan</h1>";
+    echo "Nama saya Reynaldy Salam
+    Saya tinggal di Kemayoran Jakarta Pusat
+    olahraga yang saya sukai bermain basket";
+  }
+}
